9906532866cc093989c485b4f8411e7b9243966f
mitsuaki kuwahara <quwahara@gmail.com>
2015-07-28 14:02:41 +0900

    Add Rhino version

--- src/test/java/HogeTest.java
+++ src/test/java/HogeTest.java
@@ -5,6 +5,8 @@ import javax.script.ScriptEngineManager;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.Resource;
 
@@ -35,4 +37,34 @@ public class HogeTest {
             appCtx.close();
         }
     }
+
+    @Test
+    public void testWithRhino() throws Exception {
+        ClassPathXmlApplicationContext appCtx = new ClassPathXmlApplicationContext();
+        try {
+            Context cx = Context.enter();
+            try {
+                Scriptable scope = cx.initStandardObjects();
+
+                Resource r = appCtx.getResource("classpath:/META-INF/resources/webjars/sugar/1.4.1/sugar-full.min.js");
+                String sugarJs = IOUtils.toString(r.getInputStream(), "UTF-8");
+
+                String input = "HogeTest";
+                String s = "";
+                s += sugarJs + "\n";
+                s += "var input = '" + input + "'" + "\n";
+                s += "var actual = input.dasherize()" + "\n";
+
+                cx.evaluateString(scope, s, "<cmd>", 1, null);
+
+                assertEquals("hoge-test", scope.get("actual", null));
+
+            } finally {
+                Context.exit();
+            }
+        } finally {
+            appCtx.close();
+        }
+    }
+
 }
